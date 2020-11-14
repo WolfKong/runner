456ce5b23b3b8dd63bcbd157345e2fe09ffd728f
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-11-14 14:48:48 -0300

    SideStep logic

--- Runner/Assets/Scripts/Runner.cs
+++ Runner/Assets/Scripts/Runner.cs
@@ -2,32 +2,66 @@
 
 public class Runner : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float sideStepSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravity;
+    [SerializeField] private float lineDistance;
     [SerializeField] private CharacterController characterController;
 
     private Vector3 direction;
+    private int currentLine;
 
     private void Start()
     {
         MicInput.MicInputEvent += OnMicInput;
-        TouchInput.UpInputEvent += OnTouchInput;
-        direction = new Vector3(0, 0, speed);
+        TouchInput.UpInputEvent += OnUpInput;
+        TouchInput.LeftInputEvent += OnLeftInput;
+        TouchInput.RightInputEvent += OnRightInput;
+        direction = new Vector3(0, 0, forwardSpeed);
+        currentLine = 0;
     }
 
     private void OnDestroy()
     {
         MicInput.MicInputEvent -= OnMicInput;
-        TouchInput.UpInputEvent -= OnTouchInput;
+        TouchInput.UpInputEvent -= OnUpInput;
+        TouchInput.LeftInputEvent -= OnLeftInput;
+        TouchInput.RightInputEvent -= OnRightInput;
     }
 
     private void OnMicInput() => Jump();
-    private void OnTouchInput() => Jump();
+    private void OnUpInput() => Jump();
+
+    private void OnLeftInput()
+    {
+        if (currentLine > -1)
+        {
+            currentLine--;
+            ChangeLine();
+        }
+    }
+
+    private void OnRightInput()
+    {
+        if (currentLine < 1)
+        {
+            currentLine++;
+            ChangeLine();
+        }
+    }
 
     private void Jump()
     {
-        direction.y = jumpForce;
+        if (characterController.isGrounded)
+            direction.y = jumpForce;
+    }
+
+    private void ChangeLine()
+    {
+        var targetXDirection = transform.position.x > currentLine * lineDistance ? -1 : 1;
+
+        direction.x = targetXDirection * sideStepSpeed;
     }
 
     private void Update()
@@ -38,6 +72,9 @@ public class Runner : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (Mathf.Approximately(transform.position.x, currentLine * lineDistance))
+            direction.x = 0;
+
         characterController.Move(direction * Time.fixedDeltaTime);
     }
 }
