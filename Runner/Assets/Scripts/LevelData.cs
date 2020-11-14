bd5ad99e4cbceeee24884898f598667761f8afdd
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-11-14 15:47:40 -0300

    LevelData

---
+++ Runner/Assets/Scripts/LevelData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
+[Serializable]
+public class LevelData : ScriptableObject
+{
+    public Material FloorMaterial;
+    public Material ObstacleMaterial;
+    public Material EnemyMaterial;
+
+    public override string ToString()
+    {
+        return $"Floor:{FloorMaterial} Obstacle:{ObstacleMaterial} Enemy:{EnemyMaterial}";
+    }
+}
