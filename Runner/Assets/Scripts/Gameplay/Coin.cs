838ed94dd93bddac04033121633c4275dff79ea6
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-11-15 18:40:56 -0300

    Rotating Coin cilinder

--- Runner/Assets/Scripts/Gameplay/Coin.cs
+++ Runner/Assets/Scripts/Gameplay/Coin.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] private FloatVariable playerScore;
+
+    private void Start()
+    {
+        transform.localEulerAngles = 90 * Vector3.forward;
+        transform.DOLocalRotate(new Vector3(0, 360, 90), 1, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1);
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
             playerScore.Value++;
+            transform.DOKill();
             Destroy(gameObject);
         }
     }
