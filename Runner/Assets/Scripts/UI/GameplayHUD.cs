cb315c69cf36d5b3a9f8f5106d7f6d95cc4bf0b7
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-11-15 19:07:02 -0300

    GameplayHUD with target score

---
+++ Runner/Assets/Scripts/UI/GameplayHUD.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameplayHUD : MonoBehaviour
+{
+    [SerializeField] private GameplayData gameplayData;
+    [SerializeField] private Text targetScore;
+
+    private void Start()
+    {
+        targetScore.text = $"Target: {gameplayData.LevelData.TargetScore}";
+    }
+}
