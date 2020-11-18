945eaed86b54c5ce03091c03133a43ec8524a773
Paulo Vianna <paulovianna.lwk@gmail.com>
2020-11-17 22:42:04 -0300

    Tutorial Popup

--- Runner/Assets/Scripts/UI/GameplayHUD.cs
+++ Runner/Assets/Scripts/UI/GameplayHUD.cs
@@ -1,13 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameplayHUD : MonoBehaviour
 {
     [SerializeField] private GameplayData gameplayData;
     [SerializeField] private Text targetScore;
+    [SerializeField] private GameObject tutorialPopup;
 
     private void Start()
     {
         targetScore.text = $"Target: {gameplayData.LevelData.TargetScore}";
+
+        if (PlayerPrefs.GetInt("TutorialComplete") == 0)
+            StartCoroutine(ShowTutorial());
+    }
+
+    private IEnumerator ShowTutorial()
+    {
+        yield return new WaitForSeconds(0.6f);
+
+        Instantiate(tutorialPopup, transform.parent);
+        Time.timeScale = 0;
     }
 }
---
+++ Runner/Assets/Scripts/UI/TutorialPopup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPopup : MonoBehaviour
+{
+    [SerializeField] private Button okButton;
+    [SerializeField] private Toggle toggle;
+
+    private void Start()
+    {
+        okButton.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        PlayerPrefs.SetInt("TutorialComplete", toggle.isOn ? 1 : 0);
+        Destroy(gameObject);
+        Time.timeScale = 1;
+    }
+}
